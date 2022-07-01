3870ca792663f3a55b03a0d93b7c94970b00b3bd
Inwoo Hwang <hsjkdss228@naver.com>
2022-07-01 11:56:15 +0900

    8주차 금요일 코딩 테스트: 다트 게임 (4)
    - 각 세트의 결과값을 담는 List 변수명 수정

--- 220701/Dart.java
+++ 220701/Dart.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Dart {
   public int solution(String dartResult) {
-    List<Integer> sets = new ArrayList<>();
+    List<Integer> answerSets = new ArrayList<>();
 
     int score = 0;
     int bonus = 0;
@@ -40,28 +40,31 @@ public class Dart {
       //점수, 보너스가 모두 체크되었으면 보너스와 점수만 고려해서
       //계산한 결과를 세트 하나로 해서 결과 배열 값에 추가
       if (scoreIsChecked && bonusIsChecked) {
-        sets.add((int) Math.pow(score, bonus));
+        answerSets.add((int) Math.pow(score, bonus));
 
         scoreIsChecked = false;
         bonusIsChecked = false;
       }
 
       //옵션이 있으면 결과 배열 값들에 옵션을 반영
-      int lastIndexOfSets = sets.size() - 1;
+      int lastIndexOfAnswerSets = answerSets.size() - 1;
 
       if (i + 1 < dartResult.length()) {
         switch (dartResult.charAt(i + 1)) {
           case '*' -> {
-            if (sets.size() >= 2) {
-              sets.set(lastIndexOfSets - 1, sets.get(lastIndexOfSets - 1) * 2);
+            if (answerSets.size() >= 2) {
+              answerSets.set(lastIndexOfAnswerSets - 1,
+                  answerSets.get(lastIndexOfAnswerSets - 1) * 2);
             }
 
-            sets.set(lastIndexOfSets, sets.get(lastIndexOfSets) * 2);
+            answerSets.set(lastIndexOfAnswerSets,
+                answerSets.get(lastIndexOfAnswerSets) * 2);
 
             i += 1;
           }
           case '#' -> {
-            sets.set(lastIndexOfSets, sets.get(lastIndexOfSets) * -1);
+            answerSets.set(lastIndexOfAnswerSets,
+                answerSets.get(lastIndexOfAnswerSets) * -1);
 
             i += 1;
           }
@@ -69,7 +72,7 @@ public class Dart {
       }
     }
 
-    return sets.stream()
+    return answerSets.stream()
         .mapToInt(set -> set)
         .sum();
   }
