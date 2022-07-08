6ced1126dfde3b074fe5581e22b2695144ebd8f4
Inwoo Hwang <hsjkdss228@naver.com>
2022-07-08 13:54:55 +0900

    3주차 (2) 금요일 코딩 테스트: 비밀 지도 (2)
    - 주석 설명 내용 추가

--- 220708/SecretMapTest.java
+++ 220708/SecretMapTest.java
@@ -13,6 +13,13 @@ class SecretMapTest {
     assertEquals("11100", testApplication.decimalToBinary(28, 5));
     assertEquals("10010", testApplication.decimalToBinary(18, 5));
     assertEquals("01011", testApplication.decimalToBinary(11, 5));
+
+    assertEquals("011", testApplication.decimalToBinary(3, 3));
+    assertEquals("110", testApplication.decimalToBinary(6, 3));
+    assertEquals("101", testApplication.decimalToBinary(5, 3));
+    assertEquals("001", testApplication.decimalToBinary(1, 3));
+    assertEquals("111", testApplication.decimalToBinary(7, 3));
+    assertEquals("100", testApplication.decimalToBinary(4, 3));
   }
 
   @Test
@@ -33,7 +40,7 @@ class SecretMapTest {
         "10001",
         "11100"
     };
-    String[] expected = new String[]{
+    String[] expected1 = new String[]{
         "#####",
         "# # #",
         "### #",
@@ -42,8 +49,29 @@ class SecretMapTest {
     };
 
     assertArrayEquals(
-        expected,
+        expected1,
         testApplication.makeSecretMap(5, binaryArray1, binaryArray2)
+    );
+
+    String[] binaryArray3 = new String[]{
+        "011",
+        "110",
+        "101"
+    };
+    String[] binaryArray4 = new String[]{
+        "001",
+        "111",
+        "100"
+    };
+    String[] expected2 = new String[]{
+        " ##",
+        "###",
+        "# #"
+    };
+
+    assertArrayEquals(
+        expected2,
+        testApplication.makeSecretMap(3, binaryArray3, binaryArray4)
     );
   }
 
