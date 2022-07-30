0f01b6f3e6eca5323dd34eeee7fff4d7b9157624
Inwoo Hwang <hsjkdss228@naver.com>
2022-07-30 22:27:04 +0900

    6주차 (2) 목요일 코딩 도장: 체육복 (2) (미해결)

--- 220728/Uniform.java
+++ 220728/Uniform.java
@@ -1,78 +1,67 @@
-// 예시
-// 전체 학생: 8명
-// 자기 물건 하나 간수 못하는 바보들: 1 3 4 6 7
-// 여벌의 체육복을 가져온 학생: 2 5 6
-// 여벌 체육복 가져온 학생이 빌려줬는지 확인하는 boolean 배열 두기
+// 문제 풀이 과정
+// - 여벌 체육복을 가진 학생 중 도난당한 학생이 있는지 확인
 //
-// 먼저 여벌의 체육복을 가져온 학생 중 바보가 있는지 확인
-// 여벌의 체육복을 가져왔는데 바보면 여벌을 빌려준 상태를 true로 전환
 //
-// 바보들 수만큼 반복
-//  참여 가능 학생을 일단 8명으로 두고 시작
-//  1번 바보:
-//    (앞번호는 없고) 2번 학생 확인 >> 2번 학생 여벌 있음 ㅇㅋ
-//    2번 학생 여벌 빌려준 것으로 체크, 참여 가능 줄이지 않음 (참여 가능 8)
-//  3번 바보:
-//    2번 학생, 4번 학생 확인 >> 여벌 없음 gg
-//    참여 가능 1 줄임 (참여 가능 7)
-//  4번 바보:
-//    3번 학생, 5번 학생 확인 >> 5번 학생 여벌 있음 ㅇㅋ
-//    참여 가능 줄이지 않음 (참여 가능 7)
-//  6번 바보:
-//    참여 가능 줄이지 않음 (참여 가능 7)
-//  7번 바보:
-//    본인은 여벌 안 가져옴
-//    6번 학생, 8번 학생 확인 >> 여벌 없음 gg
-//    참여 가능 1 줄임 (참여 가능 6)
+// - 체육복을 도난당한 학생 각각에 대해
+// - 앞뒤 학생 중 여벌이 있는 학생이 있는지 확인
+//   있으면 여벌이 있는 학생의 여벌 보유 여부를 false로 변경 (해당 학생의 여벌을 빌림)
+// - 앞뒤 학생 중 여벌이 없으면 본인이 여벌이 있는지 확인
+//   있으면 본인의 여벌 보유 여부를 false로 변경 (본인이 본인의 여벌을 빌림)
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Uniform {
+  // 여벌을 가진 학생이 여벌을 빌려줬는지 체크하는 맵 변수
+  private Map<Integer, Boolean> hasLentClothes;
+  List<Integer> haveExtraButLost;
+
   public int solution(
-      int studentNumber, int[] lost, int[] stockedExtra) {
-    int studentNumberCanTakeClasses = studentNumber;
+      int studentNumber,
+      int[] studentsLostClothes, int[] studentsHasExtraClothes) {
+    int studentsCountCanTakeClasses = studentNumber - studentsLostClothes.length;
 
-    boolean[] lentClothes = new boolean[stockedExtra.length];
+    hasLentClothes = new HashMap<>();
+    haveExtraButLost = new ArrayList<>();
 
-    Arrays.fill(lentClothes, false);
+    for (int studentHasExtraClothes : studentsHasExtraClothes) {
+      boolean haveExtraButLost = false;
 
-    // 여벌을 가져왔는데 바보인 학생이 있는지 확인
-    // 있으면 못 빌려주는 상태로 전환
-    for (int i = 0; i < lost.length; i += 1) {
-      for (int j = 0; j < stockedExtra.length; j += 1) {
-        if (lost[i] == stockedExtra[j]) {
-          lentClothes[j] = true;
+      for (int studentLostClothes : studentsLostClothes) {
+        if (studentHasExtraClothes == studentLostClothes) {
+          haveExtraButLost = true;
+
+          this.haveExtraButLost.add(studentHasExtraClothes);
+
+          studentsCountCanTakeClasses += 1;
           break;
         }
+      }
+
+     if (!haveExtraButLost) {
+       hasLentClothes.put(studentHasExtraClothes, false);
+     }
+    }
+
+    for (int studentLostClothes : studentsLostClothes) {
+      if (hasLentClothes.containsKey(studentLostClothes - 1)
+          && !haveExtraButLost.contains(studentLostClothes - 1)
+          && !hasLentClothes.get(studentLostClothes - 1)) {
+        hasLentClothes.replace(studentLostClothes - 1, true);
+        studentsCountCanTakeClasses += 1;
+        continue;
+      }
+
+      if (hasLentClothes.containsKey(studentLostClothes + 1)
+          && !haveExtraButLost.contains(studentLostClothes + 1)
+          && !hasLentClothes.get(studentLostClothes + 1)) {
+        hasLentClothes.replace(studentLostClothes + 1, true);
+        studentsCountCanTakeClasses += 1;
       }
     }
 
-    for (int studentIndex : lost) {
-      boolean canTakeClasses = false;
-
-      for (int i = 0; i < stockedExtra.length; i += 1) {
-        // 본인이 여벌을 가져온 바보였으면 어쨌든 본인은 참가 가능
-        if (stockedExtra[i] == studentIndex) {
-          canTakeClasses = true;
-          break;
-        }
-
-        // 앞뒤 학생이 여벌이 있는지 확인
-        // 여벌이 있으면 참석 가능하고 해당 여벌은 빌린 것으로 처리
-        if ((stockedExtra[i] == studentIndex - 1
-            || stockedExtra[i] == studentIndex + 1)
-            && !lentClothes[i]) {
-          lentClothes[i] = true;
-          canTakeClasses = true;
-          break;
-        }
-      }
-
-      if (!canTakeClasses) {
-        studentNumberCanTakeClasses -= 1;
-      }
-    }
-
-    return studentNumberCanTakeClasses;
+    return studentsCountCanTakeClasses;
   }
 }
--- 220728/UniformTest.java
+++ 220728/UniformTest.java
@@ -8,6 +8,15 @@ class UniformTest {
     Uniform test = new Uniform();
 
     assertEquals(
+        28,
+        test.solution(
+            30,
+            new int[]{1, 2, 3},
+            new int[]{4, 5, 6, 7}
+        )
+    );
+
+    assertEquals(
         6,
         test.solution(8, new int[]{1, 3, 4, 6, 7}, new int[]{2, 5, 6})
     );
@@ -30,6 +39,27 @@ class UniformTest {
     assertEquals(
         8,
         test.solution(9, new int[]{5, 6, 8, 1, 2}, new int[]{2, 3, 1, 4, 8, 9})
+    );
+
+    assertEquals(
+        4,
+        test.solution(5, new int[]{1, 2}, new int[]{2, 3})
+    );
+
+    assertEquals(5,test.solution(5,new int[] {1,2},new int[] {1,2}));
+    assertEquals(4,test.solution(5,new int[] {2,3,4},new int[] {3,5}));
+    assertEquals(2,test.solution(3,new int[] {1,2},new int[] {2,3}));
+    assertEquals(8,test.solution(9,new int[] {5,6,8,1,2},new int[] {2,3,1,4,8,9}));
+    assertEquals(6,test.solution(6,new int[] {2,4,5},new int[] {1,3,6}));
+    assertEquals(5,test.solution(5,new int[] {2,4,6},new int[] {1,3,5,7}));
+
+    assertEquals(
+        10,
+        test.solution(
+            10,
+            new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+            new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+        )
     );
   }
 }
