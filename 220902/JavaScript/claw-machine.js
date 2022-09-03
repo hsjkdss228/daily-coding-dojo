28cba7047caeae7b7572d0c0bd6c38dd5f0fe51e
Inwoo Hwang <hsjkdss228@naver.com>
2022-09-03 21:40:11 +0900

    11주차 금요일 코딩 테스트: 크레인 인형뽑기 게임 (4)
    JavaScript 완성된 풀이 추가

--- 220902/JavaScript/claw-machine.js
+++ 220902/JavaScript/claw-machine.js
@@ -3,13 +3,13 @@
 // 문제 해결 과정
 // 1. Java에서는 for문에서 배열을 종으로 참조하기 위해 인덱스 변수값을 계속해서 바꿔가며 사용했다.
 //    JavaScript에서는 선언형 방식을 사용하기 위해 foreach를 이용해 배열을 횡으로 참조할 수 있도록
+//    배열의 횡방향과 종방향을 바꾼다.
 //
-// (아래 내용은 일단 Java 풀이에서 그대로 가져왔으나 구조는 거의 그대로 가되 선언형 풀이 방식에 맞게 조금씩 바뀔 예정)
 // 2. 이제 아래 과정을 moves의 모든 숫자들에 대해 순차적으로 수행한다.
 //   2-1. moves에서 숫자를 하나 가져온다.
-//   2-2. board 배열을 횡으로 돌면서 0이 아닌 숫자를 찾으면  (숫자 - 1인 이유: 인덱스니까!)
+//   2-2. board 배열의 숫자에 해당하는 row를 보면서 0이 아닌 숫자를 찾으면
 //      그 숫자를 다른 List에 넣고 board에서 찾은 위치에는 그 숫자 대신 0을 재할당
-//   2-3. List에서 앞엣놈랑 방금 List에 넣은 놈이랑 같은지 검사한다. ([size - 2] ?= [size - 1])
+//   2-3. List에서 앞엣놈랑 방금 List에 넣은 놈이랑 같은지 검사한다. ([size - 2] ?== [size - 1])
 //      같다? 방금 List에 넣은 놈이랑 앞엣놈을 제거하고, 제거한 인형 숫자 변수에 + 2
 // 제거한 인형 숫자 변수를 반환한다.
 
@@ -17,7 +17,7 @@
 // https://www.zerocho.com/category/JavaScript/post/5acafb05f24445001b8d796d
 // https://velog.io/@dyongdi/JS-2%EC%B0%A8%EC%9B%90-%EB%B0%B0%EC%97%B4%EC%9D%98-%ED%96%89%EA%B3%BC-%EC%97%B4-%EB%B0%94%EA%BE%B8%EA%B8%B0-Transposing-a-2D-array-in-JavaScript
 
-export default function transpose(board) {
+export function transpose(board) {
   return board.reduce((newBoard, row) => (
     row.map((_, index) => (
       [
@@ -27,15 +27,44 @@ export default function transpose(board) {
   ), []);
 }
 
-function solution(board, moves) {
+export function pick(boardTransposed, pickedThings, row) {
+  const pickedThing = boardTransposed[row - 1].find((element) => element > 0);
+
+  if (pickedThing === undefined) {
+    return;
+  }
+
+  pickedThings.push(pickedThing);
+
+  const indexOfPickedThing = boardTransposed[row - 1].findIndex((element) => element > 0);
+  boardTransposed[row - 1][indexOfPickedThing] = 0;
+}
+
+export function checkPickedThings(pickedThings) {
+  if (pickedThings.length > 1
+    && pickedThings[pickedThings.length - 1] === pickedThings[pickedThings.length - 2]) {
+    pickedThings.pop();
+    pickedThings.pop();
+
+    return 2;
+  }
+
+  return 0;
+}
+
+export function solution(board, moves) {
   const boardTransposed = transpose(board);
+
+  const pickedThings = [];
 
   const removedCount = {
     count: 0,
   };
 
-  moves.foreach((column) => {
-    console.log(column);
+  moves.forEach((row) => {
+    pick(boardTransposed, pickedThings, row);
+
+    removedCount.count += checkPickedThings(pickedThings);
   });
 
   return removedCount.count;
--- 220902/JavaScript/claw-machine.test.js
+++ 220902/JavaScript/claw-machine.test.js
@@ -1,19 +1,80 @@
-import transpose from './claw-machine';
+import {
+  transpose, checkPickedThings, pick, solution,
+} from './claw-machine';
 
-test('transpose', () => {
+test('Transpose', () => {
   const array = [
-    [1, 2, 3, 999],
-    [4, 5, 6, 7],
-    [8, 9, 10, 11],
-    [12, 13, 14, 15],
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 3],
+    [0, 2, 5, 0, 1],
+    [4, 2, 4, 4, 2],
+    [3, 5, 1, 3, 1],
   ];
 
   const arrayExpect = [
-    [1, 4, 8, 12],
-    [2, 5, 9, 13],
-    [3, 6, 10, 14],
-    [999, 7, 11, 15],
+    [0, 0, 0, 4, 3],
+    [0, 0, 2, 2, 5],
+    [0, 1, 5, 4, 1],
+    [0, 0, 0, 4, 3],
+    [0, 3, 1, 2, 1],
   ];
 
   expect(transpose(array)).toStrictEqual(arrayExpect);
+});
+
+test('pick', () => {
+  const board = [
+    [0, 0, 0, 4, 3],
+    [0, 0, 2, 2, 5],
+    [0, 1, 5, 4, 1],
+    [0, 0, 0, 4, 3],
+    [0, 3, 1, 2, 1],
+  ];
+
+  const pickedThings = [1, 2, 3];
+
+  const row = 3;
+
+  pick(board, pickedThings, row);
+
+  expect(pickedThings.length).toBe(4);
+  expect(pickedThings.pop()).toBe(1);
+});
+
+test('Check picked things when being deleted', () => {
+  const pickedThings = [1, 3, 4, 2, 3, 3];
+
+  const removedCount = {
+    count: 0,
+  };
+
+  removedCount.count += checkPickedThings(pickedThings);
+
+  expect(removedCount.count).toBe(2);
+});
+
+test('Check picked things when not being deleted', () => {
+  const pickedThings = [1, 3, 4, 2, 3, 2];
+
+  const removedCount = {
+    count: 0,
+  };
+
+  removedCount.count += checkPickedThings(pickedThings);
+
+  expect(removedCount.count).toBe(0);
+});
+
+test('Solution', () => {
+  const board = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 3],
+    [0, 2, 5, 0, 1],
+    [4, 2, 4, 4, 2],
+    [3, 5, 1, 3, 1],
+  ];
+
+  const moves = [1, 5, 3, 5, 1, 2, 1, 4];
+
+  expect(solution(board, moves)).toBe(4);
 });
