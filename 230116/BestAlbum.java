7a1cf2282bad8a4f404797118538ffb09010fcbd
Inwoo Hwang <hsjkdss228@naver.com>
2023-01-17 10:08:05 +0900

    23년 1월 3주차 코딩 도장: 베스트앨범 (2) (미해결)
    Java 풀이 중 (2) (풀이 지정 시간 초과)

--- 230116/BestAlbum.java
+++ 230116/BestAlbum.java
@@ -1,23 +1,29 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class BestAlbum {
     private Map<Integer, String> indexAndGenres = new HashMap<>();
     private Map<Integer, Integer> indexAndPlays = new HashMap<>();
     private Map<String, Integer> genreAndPlaySums = new HashMap<>();
+    private List<Map.Entry<String, Integer>> sortedGenres = new ArrayList<>();
+    private List<Integer> indices = new ArrayList<>();
 
     public int[] solution(String[] genres, int[] plays) {
         setIndexAndGenresPlays(genres, plays);
         setGenresAndPlaySums(genres, plays);
 
-        List<Map.Entry<String, Integer>> sortedGenres = sortGenres(genreAndPlaySums);
+        sortGenres(genreAndPlaySums);
 
-        int[] answer = {};
-        return answer;
+        addIndices();
+
+        return indices.stream()
+            .mapToInt(index -> index)
+            .toArray();
     }
 
     public Map<Integer, String> indexAndGenres() {
@@ -28,8 +34,16 @@ public class BestAlbum {
         return indexAndPlays;
     }
 
+    public List<Map.Entry<String, Integer>> sortedGenres() {
+        return sortedGenres;
+    }
+
     public Map<String, Integer> genreAndPlaySums() {
         return genreAndPlaySums;
+    }
+
+    public List<Integer> indices() {
+        return indices;
     }
 
     public void setIndexAndGenresPlays(String[] genres, int[] plays) {
@@ -55,10 +69,79 @@ public class BestAlbum {
         }
     }
 
-    public List<Map.Entry<String, Integer>> sortGenres(Map<String, Integer> genreAndPlaySums) {
-        List<Map.Entry<String, Integer>> sortedGenres
-            = new ArrayList<>(genreAndPlaySums.entrySet());
-        sortedGenres.sort((previous, next) -> next.getValue().compareTo(previous.getValue()));
-        return sortedGenres;
+    public void sortGenres(
+        Map<String, Integer> genreAndPlaySums
+    ) {
+        sortedGenres = new ArrayList<>(genreAndPlaySums.entrySet());
+        // 오름차순 정렬
+        sortedGenres.sort((previous, next) ->
+            next.getValue().compareTo(previous.getValue())
+        );
+    }
+
+    public void addIndices() {
+        for (Map.Entry<String, Integer> genreAndPlaySum : sortedGenres) {
+            String genre = genreAndPlaySum.getKey();
+
+            Set<Map.Entry<Integer, String>> indexAndSelectedGenres
+                = indexAndGenres.entrySet().stream()
+                .filter(indexAndGenre -> indexAndGenre.getValue().equals(genre))
+                .collect(Collectors.toSet());
+            List<Map.Entry<Integer, Integer>> indexAndSelectedPlays
+                = new ArrayList<>();
+            indexAndSelectedGenres.forEach(indexAndSelectedGenre -> {
+                Integer index = indexAndSelectedGenre.getKey();
+                indexAndSelectedPlays.add(
+                    Map.entry(index, indexAndPlays.get(index))
+                );
+            });
+
+            // 장르의 곡이 하나만 있으면 index를 그냥 추가
+            if (indexAndSelectedPlays.size() == 1) {
+                indices.add(indexAndSelectedPlays.get(0).getKey());
+                continue;
+            }
+
+            sortPlays(indexAndSelectedPlays);
+
+            Map.Entry<Integer, Integer> selectedIndexAndPlay1
+                = indexAndSelectedPlays.get(0);
+            Map.Entry<Integer, Integer> selectedIndexAndPlay2
+                = indexAndSelectedPlays.get(1);
+
+            Integer selectedIndex1 = selectedIndexAndPlay1.getKey();
+            Integer selectedIndex2 = selectedIndexAndPlay2.getKey();
+            Integer selectedPlay1 = selectedIndexAndPlay1.getValue();
+            Integer selectedPlay2 = selectedIndexAndPlay2.getValue();
+
+            System.out.println("selectedIndex1: " + selectedIndex1);
+            System.out.println("selectedIndex2: " + selectedIndex2);
+            System.out.println("selectedPlay1: " + selectedPlay1);
+            System.out.println("selectedPlay2: " + selectedPlay2);
+
+            // 추가할 두 곡의 재생 횟수가 같으면 index가 작은 쪽을 먼저 push
+            if (selectedPlay1.equals(selectedPlay2)) {
+                int first = Math.min(selectedIndex1, selectedIndex2);
+                int second = Math.max(selectedIndex1, selectedIndex2);
+                indices.add(first);
+                indices.add(second);
+                continue;
+            }
+
+            // edge case에 해당되지 않으면 선택된 index 순서대로 push
+            indices.add(selectedIndex1);
+            indices.add(selectedIndex2);
+        }
+    }
+
+    public void sortPlays(
+        List<Map.Entry<Integer, Integer>> indexAndSelectedPlays
+    ) {
+        // 내림차순 정렬
+        indexAndSelectedPlays.sort((previous, next) -> {
+            Integer previousPlays = previous.getValue();
+            Integer nextPlays = next.getValue();
+            return previousPlays.compareTo(nextPlays);
+        });
     }
 }
--- 230116/BestAlbumTest.java
+++ 230116/BestAlbumTest.java
@@ -40,17 +40,43 @@ class BestAlbumTest {
     @Test
     void sortGenres() {
         BestAlbum test = new BestAlbum();
+
         Map<String, Integer> genreAndPlaySums = Map.of(
             "jazz", 1200,
             "classic", 5000,
             "k-pop", 300,
             "j-pop", 2600
         );
-        List<Map.Entry<String, Integer>> sortedGenres
-            = test.sortGenres(genreAndPlaySums);
+        test.sortGenres(genreAndPlaySums);
+        List<Map.Entry<String, Integer>> sortedGenres = test.sortedGenres();
         assertThat(sortedGenres.get(0).getKey()).isEqualTo("classic");
         assertThat(sortedGenres.get(1).getKey()).isEqualTo("j-pop");
         assertThat(sortedGenres.get(2).getKey()).isEqualTo("jazz");
         assertThat(sortedGenres.get(3).getKey()).isEqualTo("k-pop");
+    }
+
+    @Test
+    void addIndices() {
+        BestAlbum test = new BestAlbum();
+
+        String[] genres = new String[]{
+            "classic", "pop", "classic", "classic", "pop"
+        };
+        int[] plays = new int[]{
+            500, 600, 150, 800, 2500
+        };
+        Map<String, Integer> genreAndPlaySums = Map.of(
+            "classic", 1450,
+            "pop", 3100
+        );
+        test.setIndexAndGenresPlays(genres, plays);
+        test.setGenresAndPlaySums(genres, plays);
+        test.sortGenres(genreAndPlaySums);
+        test.addIndices();
+        List<Integer> indices = test.indices();
+        assertThat(indices.get(0)).isEqualTo(4);
+        assertThat(indices.get(1)).isEqualTo(1);
+        assertThat(indices.get(2)).isEqualTo(3);
+        assertThat(indices.get(3)).isEqualTo(0);
     }
 }
